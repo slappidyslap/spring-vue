f4be0fcd9960e5207e1b1b5edaf7dd5d043ee608
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2022-10-03 15:33:10 +0600

    preparing for websocket

--- spring-vue-backend/src/main/java/io/melakuera/springvue/controllers/ZamenaController.java
+++ spring-vue-backend/src/main/java/io/melakuera/springvue/controllers/ZamenaController.java
@@ -1,2 +1,19 @@
-package io.melakuera.springvue.controllers;public class ZamenaController {
+package io.melakuera.springvue.controllers;
+
+import io.melakuera.springvue.dto.ZamenaOfferWebSocketRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class ZamenaController {
+
+    @MessageMapping("/zamena")
+    @SendTo("/topic/zamenas")
+    public void offerZamena(@Payload ZamenaOfferWebSocketRequest zamenaOffer) {
+        log.info(zamenaOffer.toString());
+    }
 }
